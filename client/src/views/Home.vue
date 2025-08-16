ebc35e3d1e9312ce83f535b890f9bdabdea84ee7
rokhomenko <roksa.khom@gmail.com>
2025-08-16 23:59:28 +0200

    add header component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,4 +1,9 @@
+<script setup>
+import AppHeader from '@/components/AppHeader.vue'
+</script>
+
 <template>
+  <AppHeader/>
   <div>Home</div>
   <router-link to="/login">login</router-link>
   <router-link to="/register">register</router-link>
