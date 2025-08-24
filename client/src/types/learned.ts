1aad3ed658a900eede83d21b4ff644fa55dda4d1
rokhomenko <roksa.khom@gmail.com>
2025-08-24 22:54:33 +0200

    add learnedMap

--- client/src/types/learned.ts
+++ client/src/types/learned.ts
@@ -7,6 +7,7 @@ export interface LearnedGroup {
 
 export interface LearnedGroupsState {
   learned_groups: LearnedGroup[]
+  learnedMap: Record<number, number>
   loading: boolean
   error: string | null
 }
