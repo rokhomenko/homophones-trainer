92c10ea04a1bcff3936917254f5b96195bb8c7f7
rokhomenko <roksa.khom@gmail.com>
2025-08-25 22:33:57 +0200

    separate learned groups with words for training

--- client/src/stores/learned.ts
+++ client/src/stores/learned.ts
@@ -1,13 +1,60 @@
 import { defineStore } from "pinia"
+import { useGroupsStore } from "./groups"
+import { useWordsStore } from "./words"
+import { useAuthStore } from "./auth"
 import axios from 'axios'
 import type { LearnedGroupsState, LearnedGroup } from "@/types/learned"
+import type { GroupedWords } from "@/types/words"
+import type { Group } from "@/types/groups"
 
 export const useLearnedsStore = defineStore('learned', {
   state: (): LearnedGroupsState => ({
     learned_groups: [],
+    learnedMap: {},
     loading: false,
     error: null
   }),
+
+  getters: {
+    separateLearningGroups: (state): {learnedWords: any[], wordsToLearn: any[]} => {
+      const groupStore = useGroupsStore()
+      const wordsStore = useWordsStore()
+      const authStore = useAuthStore()
+      if (!authStore.user) {
+        throw Error('User must be authenticated to access learning groups')
+      }
+      const userId = authStore.user.id
+
+      const allWords = groupStore.groups.filter(g => g.additional_to_id === 0)
+
+      const wordsByGroup = wordsStore.words.reduce((acc, word) => {
+        if(!acc[word.group_id]) acc[word.group_id] = []
+        acc[word.group_id].push(word)
+        return acc
+      }, {} as GroupedWords)
+
+      const learnedGroupsId = state.learnedMap[userId] || []
+
+      const learnedWords = allWords
+        .filter(g => learnedGroupsId.includes(g.id))
+        .map(g => ({
+          ...g,
+          words: wordsByGroup[g.id] || []
+        }))
+
+      const wordsToLearn = allWords
+        .filter(g => !learnedGroupsId.includes(g.id))
+        .map(g => ({
+          ...g,
+          words: wordsByGroup[g.id] || []
+        }))
+
+        return {
+          learnedWords,
+          wordsToLearn
+        }
+    }
+  },
 
   actions: {
     async fetchLearned() {
@@ -16,6 +63,14 @@ export const useLearnedsStore = defineStore('learned', {
       try {
         const res = await axios.get<LearnedGroup[]>('https://x8ki-letl-twmt.n7.xano.io/api:PKgvb2gt/learned_groups')
         this.learned_groups = res.data
+
+        this.learnedMap = res.data.reduce((acc: Record<number, number[]>, group) => {
+          if(!acc[group.user_id]) acc[group.user_id] = []
+          if(!acc[group.user_id].includes(group.group_id)) {
+            acc[group.user_id].push(group.group_id)
+          }
+          return acc
+        }, {} as Record<number, number[]>)
       } catch (err: any) {
         this.error = err.message || 'Error fetching learned groups'
       } finally {
--- client/src/views/Training.vue
+++ client/src/views/Training.vue
@@ -1,3 +1,36 @@
+<script setup lang="ts">
+  import { computed, onMounted } from 'vue'
+  import { useWordsStore } from '@/stores/words'
+  import { useGroupsStore } from '@/stores/groups'
+  import { useLearnedsStore } from '@/stores/learned'
+
+  const wordsStore = useWordsStore()
+  const groupsStore = useGroupsStore()
+  const learnedStore = useLearnedsStore()
+
+  onMounted(async () => {
+    await Promise.all([
+      groupsStore.fetchGroups(),
+      wordsStore.fetchWords(),
+      learnedStore.fetchLearned()
+    ])
+  })
+
+  const wordsToLearn = computed(() => learnedStore.separateLearningGroups.wordsToLearn)
+</script>
+
 <template>
-  <div>Training</div>
+  <div>
+    <ul
+      v-for="group in wordsToLearn"
+      :key="group.id"
+    >
+      <li
+        v-for="word in group.words"
+        :key="group.id"
+      >
+        {{ word.word }}
+      </li>
+    </ul>
+  </div>
 </template>
