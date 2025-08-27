bb094e68a823abd6949a159ea79a21dc153f10cf
rokhomenko <roksa.khom@gmail.com>
2025-08-28 01:35:14 +0200

    push to login page when  click logout

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -1,12 +1,18 @@
 <script setup>
   import { useAuthStore } from '@/stores/auth'
+  import router from '@/router'
 
   const auth = useAuthStore()
+
+  function logout () {
+    auth.logout()
+    router.push({ name: 'login' })
+  }
 </script>
 
 <template>
   <div>Header</div>
   <router-link to="/auth/login">login</router-link>
   <router-link to="/auth/register">register</router-link>
-  <button @click="auth.logout()" class="btn-logout">Logout</button>
+  <button @click="logout" class="btn-logout">Logout</button>
 </template>
