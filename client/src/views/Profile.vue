c9d98524b66dfd48a1a7432948611eee8c54ec93
rokhomenko <roksa.khom@gmail.com>
2025-09-02 03:02:00 +0200

    show not learned in profile

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,3 +1,38 @@
+<script setup lang="ts">
+  import { computed, onMounted } from 'vue'
+  import { useWordsStore } from '@/stores/words'
+  import { useGroupsStore } from '@/stores/groups'
+  import { useLearnedStore } from '@/stores/learned'
+  import type { GroupWithWords } from '@/types/derived'
+
+
+  const wordsStore = useWordsStore()
+  const groupsStore = useGroupsStore()
+  const learnedStore = useLearnedStore()
+
+  onMounted(async () => {
+    await Promise.all([
+      groupsStore.fetchGroups(),
+      wordsStore.fetchWords(),
+      learnedStore.fetchLearned()
+    ])
+  })
+
+  const wordsToLearn = computed<GroupWithWords[]>(() => learnedStore.separateLearningGroups.wordsToLearn)
+</script>
+
 <template>
-  <div>Profile</div>
+  <div>
+    <ul
+      v-for="group in wordsToLearn"
+      :key="group.id"
+    >
+      <li
+        v-for="word in group.words"
+        :key="group.id"
+      >
+        {{ word.word }}
+      </li>
+    </ul>
+  </div>
 </template>
