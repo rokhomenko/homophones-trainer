59d68c85e9718c10aa3a60d3131b8ee0c3858f6f
rokhomenko <roksa.khom@gmail.com>
2025-11-13 01:48:45 +0100

    show progress bar

--- client/src/components/training/TrainingProgress.vue
+++ client/src/components/training/TrainingProgress.vue
@@ -1,14 +1,33 @@
 <script setup lang="ts">
   import { useRouter } from 'vue-router'
+  import { computed,watch } from 'vue'
   import { useTrainingStore } from '@/stores/training'
   import TrainingResult from './TrainingResult.vue'
 
   const router = useRouter()
   const trainingStore = useTrainingStore()
+
+  const calculateRowWidth = computed(() => {
+    return trainingStore.showResults.map(group => {
+      return group.words.map(w => {
+        const value = w.shown ? (w.correct / 3).toFixed(1) : 0
+        return Number(value) * 100
+      })
+    })
+  })
+
+  watch(
+    calculateRowWidth,
+    (oldVal, newVal) => {
+      console.log('oldVal', oldVal)
+      console.log('newVal', newVal)
+    },
+    { deep: true}
+  )
 </script>
 
 <template>
-  <div v-for="group in trainingStore.showResults" :key="group.groupId">
+  <!-- <div v-for="group in trainingStore.showResults" :key="group.groupId">
     <table>
       <thead>
         <tr>
@@ -23,6 +42,37 @@
         </tr>
       </tbody>
     </table>
+  </div>
+  <br/> -->
+  <div class="flex flex-col">
+    <div class="flex flex-col w-full" v-for="(g, groupIndex) in trainingStore.showResults" :key="g.groupId">
+    <div v-for="(w, wordIndex) in g.words" :key="w.id">
+      <div
+        class="bg-gradient-to-r from-emerald-100 to-cyan-100 mb-3 text-white font-bold rounded-md px-2"
+        :style="{ width: calculateRowWidth[groupIndex][wordIndex] + '%' }"
+      >
+        {{ w.word }}
+      </div>
+    </div>
+  </div>
+
+    <!-- <table>
+      <thead>
+        <tr>
+          <th class="border border-gray-400 px-2 py-1">Word</th>
+          <th class="border border-gray-400 px-2 py-1">Correct</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="w in group.words" :key="w.id">
+          <td class="border border-gray-400 px-2 py-1">{{ w.word }}</td>
+          <td class="border border-gray-400 px-2 py-1">{{ w.correct }} of {{ w.shown }}</td>
+            <td class="border border-gray-400 px-2 py-1">
+            {{ w.shown ? (w.correct / 3).toFixed(1) : 0 }}
+          </td>
+        </tr>
+      </tbody>
+    </table> -->
   </div>
   <!-- <TrainingResult /> -->
 </template>
--- client/src/views/Training.vue
+++ client/src/views/Training.vue
@@ -110,41 +110,43 @@
       Start New Training
     </button>
   </div>
-  <div class="flex flex-col items-center" v-if="!trainingStore.finished">
-    <!-- <div v-if="showAnswer">
-      {{ trainingStore.currentWord?.word.word }}
-    </div> -->
-    <button v-if="trainingStore.currentWord?.word.word" @click="speak(trainingStore.currentWord?.word.word)" class="my-15">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 text-slate-500">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-      </svg>
-    </button>
-    <div class="flex flex-col md:flex-row gap-5 items-center">
-      <ul class="flex flex-row gap-5">
-        <li
-          v-for="w in trainingStore.currentWord?.group.words"
-          :key="w.id"
-          @click="selectWord(w.id)"
-          class="flex gap-2 px-3 py-2 bg-cyan-600 text-white font-bold rounded-lg cursor-pointer w-fit transition-colors"
-          :class="{
-            'bg-emerald-500' : answeredCurrentWord && !isCurrentGroupHomophones && w.id === trainingStore.currentWord?.word.id,
-            'bg-rose-600' : answeredCurrentWord && !allWordsSelected && (isCurrentGroupHomophones || (selectedWordId === w.id && w.id !== trainingStore.currentWord?.word.id))
-          }"
+  <div class="flex flex-col justify-between">
+    <div class="flex flex-col items-center" v-if="!trainingStore.finished">
+      <!-- <div v-if="showAnswer">
+        {{ trainingStore.currentWord?.word.word }}
+      </div> -->
+      <button v-if="trainingStore.currentWord?.word.word" @click="speak(trainingStore.currentWord?.word.word)" class="my-15">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-10 text-slate-500">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+        </svg>
+      </button>
+      <div class="flex flex-col md:flex-row gap-5 items-center">
+        <ul class="flex flex-row gap-5">
+          <li
+            v-for="w in trainingStore.currentWord?.group.words"
+            :key="w.id"
+            @click="selectWord(w.id)"
+            class="flex gap-2 px-3 py-2 bg-cyan-600 text-white font-bold rounded-lg cursor-pointer w-fit transition-colors"
+            :class="{
+              'bg-emerald-500' : answeredCurrentWord && !isCurrentGroupHomophones && w.id === trainingStore.currentWord?.word.id,
+              'bg-rose-600' : answeredCurrentWord && !allWordsSelected && (isCurrentGroupHomophones || (selectedWordId === w.id && w.id !== trainingStore.currentWord?.word.id))
+            }"
+          >
+            {{ w.word }}
+          </li>
+        </ul>
+        <button
+          @click="selectAllWords"
+          class="flex gap-2 px-3 py-2 bg-cyan-600 text-white font-bold rounded-lg cursor-pointer transition-colors"
+          :class="allWordsButtonClass"
         >
-          {{ w.word }}
-        </li>
-      </ul>
-      <button
-        @click="selectAllWords"
-        class="flex gap-2 px-3 py-2 bg-cyan-600 text-white font-bold rounded-lg cursor-pointer transition-colors"
-        :class="allWordsButtonClass"
-      >
-        All words sound the same
+          All words sound the same
+        </button>
+      </div>
+      <button class="mb-8 mt-20 cursor-pointer text-slate-600" @click="nextWord()" :disabled="!answeredCurrentWord">
+        {{ hasNextWord ? 'Next' : 'Finish' }}
       </button>
     </div>
-    <button class="mb-8 mt-20 cursor-pointer text-slate-600" @click="nextWord()" :disabled="!answeredCurrentWord">
-      {{ hasNextWord ? 'Next' : 'Finish' }}
-    </button>
+    <TrainingProgress />
   </div>
-  <TrainingProgress />
 </template>
