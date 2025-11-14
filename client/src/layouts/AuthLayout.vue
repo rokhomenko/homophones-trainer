abf2c992df5afbda898e8a7987eb423b649e70ea
rokhomenko <roksa.khom@gmail.com>
2025-11-14 19:42:53 +0100

    auth template

--- client/src/layouts/AuthLayout.vue
+++ client/src/layouts/AuthLayout.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <router-view />
+  <div class="min-h-screen bg-gradient-to-r from-emerald-100 to-cyan-100 flex items-center justify-center p-4">
+    <div class="flex flex-col items-center justify-center bg-white h-[50vh] lg:h-[50vh] w-[90vw] lg:w-[40vw] rounded-md p-8">
+      <router-view />
+    </div>
   </div>
 </template>
