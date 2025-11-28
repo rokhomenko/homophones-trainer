2091255c5d7ca00d15b27b91e80f0f66ba66c633
rokhomenko <roksa.khom@gmail.com>
2025-11-28 20:55:55 +0100

    lang us

--- client/src/utils/speak.ts
+++ client/src/utils/speak.ts
@@ -2,6 +2,8 @@
 export function speak(word: string): Promise<void> {
   return new Promise(resolve => {
     const utterance = new SpeechSynthesisUtterance(word)
+    utterance.lang = 'en-US'
+    utterance.rate = 0.9
     utterance.onend = () => {
       resolve();
     }
