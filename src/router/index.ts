77c1fa42696b8e73ee694769f2c635c59f3c28d1
rokhomenko <roksa.khom@gmail.com>
2026-03-11 22:36:22 +0100

    fix text

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,38 +5,58 @@ const router = createRouter({
   history: createWebHistory('/'),
   routes: [
     {
-    path: '/',
-    component: () => import('@/layouts/DefaultLayout.vue'),
-    children: [
-      { path: '', name: 'home', component: () => import('@/views/Home.vue') },
-      { path: 'profile', name: 'profile', component: () => import('@/views/Profile.vue'), meta: { requiresAuth: true } },
-      { path: 'training', name: 'training', component: () => import('@/views/Training.vue'), meta: { requiresAuth: true } }
-    ]
+      path: '/',
+      component: () => import('@/layouts/DefaultLayout.vue'),
+      children: [
+        { path: '', name: 'home', component: () => import('@/views/HomeView.vue') },
+        {
+          path: 'profile',
+          name: 'profile',
+          component: () => import('@/views/Profile.vue'),
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'training',
+          name: 'training',
+          component: () => import('@/views/Training.vue'),
+          meta: { requiresAuth: true },
+        },
+      ],
     },
     {
       path: '/auth',
       component: () => import('@/layouts/AuthLayout.vue'),
       children: [
-        { path: 'login', name: 'login', component: () => import('@/views/Login.vue'), meta: { guestOnly: true } },
-        { path: 'register', name: 'register', component: () => import('@/views/Register.vue'), meta: { guestOnly: true } }
-      ]
-    }
+        {
+          path: 'login',
+          name: 'login',
+          component: () => import('@/views/Login.vue'),
+          meta: { guestOnly: true },
+        },
+        {
+          path: 'register',
+          name: 'register',
+          component: () => import('@/views/Register.vue'),
+          meta: { guestOnly: true },
+        },
+      ],
+    },
   ],
 })
 
 router.beforeEach(async (to, from, next) => {
   const auth = useAuthStore()
 
-  if(auth.token && !auth.user) {
+  if (auth.token && !auth.user) {
     await auth.getUser()
   }
 
-  if(to.meta.guestOnly && auth.isAuthenticated) {
-    return next({name: 'home'})
+  if (to.meta.guestOnly && auth.isAuthenticated) {
+    return next({ name: 'home' })
   }
 
-  if(to.meta.requiresAuth && !auth.isAuthenticated) {
-    return next({name: 'login'})
+  if (to.meta.requiresAuth && !auth.isAuthenticated) {
+    return next({ name: 'login' })
   } else {
     next()
   }
