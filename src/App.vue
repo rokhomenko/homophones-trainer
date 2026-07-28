a715f176b43456e601620810f62095f92da0d7d5
rokhomenko <roksa.khom@gmail.com>
2026-07-28 21:06:55 +0200

    remove vercel speed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { useAuthStore } from './stores/auth'
 import { onMounted } from 'vue'
 import { Analytics } from "@vercel/analytics/vue"
-import { SpeedInsights } from "@vercel/speed-insights/vue"
 const auth = useAuthStore()
 
 onMounted(() => {
@@ -14,7 +13,6 @@ onMounted(() => {
 
 <template>
   <Analytics />
-  <SpeedInsights />
   <router-view></router-view>
 </template>
 
