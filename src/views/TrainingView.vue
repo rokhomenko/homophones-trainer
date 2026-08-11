f0a68bc4be9b115f5b12e0f99f0b134b5e6b1c37
rokhomenko <roksa.khom@gmail.com>
2026-08-11 17:32:42 +0200

    reset the training store

--- src/views/TrainingView.vue
+++ src/views/TrainingView.vue
@@ -15,6 +15,10 @@ const trainingStore = useTrainingStore()
 const isDisabled = ref(false)
 const isReady = ref(false)
 
+trainingStore.finished = false
+trainingStore.trainingQueue = []
+trainingStore.currentWordIndex = 0
+
 const showAnswer = ref(false)
 const answeredCurrentWord = ref(false)
 const selectedWordId = ref<number | null>(null)
@@ -130,50 +134,55 @@ async function handleSpeak(word: string) {
     <p class="text-xl text-stone-400">wait...</p>
   </div>
   <template v-else>
-  <div class="flex justify-center" v-if="trainingStore.finished">
-    <div class="flex flex-col items-center">
-      <TrainingResult />
-      <button
-        class="mt-5 mb-10 flex max-w-[calc(100%-30px)] items-center justify-center whitespace-nowrap border border-[#9e553a]/70 bg-[#241812] px-8 py-4 text-center text-lg font-black uppercase tracking-[0.18em] text-stone-100 shadow-[8px_8px_0_rgb(0_0_0/0.35)] hover:bg-[#2b1b14] sm:px-12 md:px-16 lg:mb-20 lg:mt-10 rounded-xl"
-        @click="startNewTraining">
-        Start New Training
-      </button>
-    </div>
-  </div>
-  <div class="flex flex-col justify-between gap-12">
-    <div class="mx-auto flex w-full max-w-4xl flex-col items-center border border-stone-800/90 bg-[#15130f]/88 px-5 py-8 text-center shadow-[0_18px_70px_rgb(0_0_0/0.28)] rounded-[2rem] sm:px-8" v-if="!trainingStore.finished">
-      <div class="text-xs font-semibold uppercase tracking-[0.28em] text-[#d99165]">Make it louder</div>
-      <div class="mt-2 text-sm text-stone-500">Tap the speaker to hear again</div>
-      <button v-if="trainingStore.currentWord?.word.word" :disabled="isDisabled"
-        @click="handleSpeak(trainingStore.currentWord?.word.word)" class="my-14 rounded-full border border-stone-700 bg-[#0f0e0c] p-8 text-stone-200 shadow-[0_0_50px_rgb(140_63_45/0.16)] hover:border-[#9e553a] hover:text-[#d99165] disabled:opacity-50">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-          class="size-12">
-          <path stroke-linecap="round" stroke-linejoin="round"
-            d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
-        </svg>
-      </button>
-      <div class="flex flex-col items-center gap-5 md:flex-row">
-        <ul class="flex flex-row flex-wrap justify-center gap-3">
-          <li v-for="w in trainingStore.currentWord?.group.words" :key="w.id" @click="selectWord(w.id)"
-            class="flex w-fit cursor-pointer gap-2 border border-stone-700 bg-[#0f0e0c] px-4 py-3 font-bold text-stone-100 transition-colors hover:border-[#9e553a] hover:text-[#d99165] rounded-xl"
-            :class="{
-              'border-[#6f7d5a] bg-[#1f281c] text-[#c9d7a5]': answeredCurrentWord && !isCurrentGroupHomophones && w.id === trainingStore.currentWord?.word.id,
-              'border-[#d99165] bg-[#3a2419] text-[#f0b180]': answeredCurrentWord && !allWordsSelected && (isCurrentGroupHomophones || (selectedWordId === w.id && w.id !== trainingStore.currentWord?.word.id))
-            }">
-            {{ w.word }}
-          </li>
-        </ul>
-        <button @click="selectAllWords"
-          class="flex cursor-pointer gap-2 border border-stone-700 bg-[#0f0e0c] px-4 py-3 font-bold text-stone-100 transition-colors hover:border-[#9e553a] hover:text-[#d99165] rounded-xl"
-          :class="allWordsButtonClass">
-          All words sound the same
+    <div class="flex justify-center" v-if="trainingStore.finished">
+      <div class="flex flex-col items-center">
+        <TrainingResult />
+        <button
+          class="mt-5 mb-10 flex max-w-[calc(100%-30px)] items-center justify-center whitespace-nowrap border border-[#9e553a]/70 bg-[#241812] px-8 py-4 text-center text-lg font-black uppercase tracking-[0.18em] text-stone-100 shadow-[8px_8px_0_rgb(0_0_0/0.35)] hover:bg-[#2b1b14] sm:px-12 md:px-16 lg:mb-20 lg:mt-10 rounded-xl"
+          @click="startNewTraining">
+          Start New Training
         </button>
       </div>
-      <button class="mb-2 mt-16 cursor-pointer text-sm font-black uppercase tracking-[0.24em] text-stone-300 hover:text-[#d99165] disabled:cursor-not-allowed disabled:opacity-30" @click="nextWord()" :disabled="!answeredCurrentWord">
-        {{ hasNextWord ? 'Next' : 'Finish' }}
-      </button>
     </div>
-    <TrainingProgress />
-  </div>
+    <div class="flex flex-col justify-between gap-12">
+      <div
+        class="mx-auto flex w-full max-w-4xl flex-col items-center border border-stone-800/90 bg-[#15130f]/88 px-5 py-8 text-center shadow-[0_18px_70px_rgb(0_0_0/0.28)] rounded-[2rem] sm:px-8"
+        v-if="!trainingStore.finished">
+        <div class="text-xs font-semibold uppercase tracking-[0.28em] text-[#d99165]">Make it louder</div>
+        <div class="mt-2 text-sm text-stone-500">Tap the speaker to hear again</div>
+        <button v-if="trainingStore.currentWord?.word.word" :disabled="isDisabled"
+          @click="handleSpeak(trainingStore.currentWord?.word.word)"
+          class="my-14 rounded-full border border-stone-700 bg-[#0f0e0c] p-8 text-stone-200 shadow-[0_0_50px_rgb(140_63_45/0.16)] hover:border-[#9e553a] hover:text-[#d99165] disabled:opacity-50">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="size-12">
+            <path stroke-linecap="round" stroke-linejoin="round"
+              d="M19.114 5.636a9 9 0 0 1 0 12.728M16.463 8.288a5.25 5.25 0 0 1 0 7.424M6.75 8.25l4.72-4.72a.75.75 0 0 1 1.28.53v15.88a.75.75 0 0 1-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.009 9.009 0 0 1 2.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75Z" />
+          </svg>
+        </button>
+        <div class="flex flex-col items-center gap-5 md:flex-row">
+          <ul class="flex flex-row flex-wrap justify-center gap-3">
+            <li v-for="w in trainingStore.currentWord?.group.words" :key="w.id" @click="selectWord(w.id)"
+              class="flex w-fit cursor-pointer gap-2 border border-stone-700 bg-[#0f0e0c] px-4 py-3 font-bold text-stone-100 transition-colors hover:border-[#9e553a] hover:text-[#d99165] rounded-xl"
+              :class="{
+                'border-[#6f7d5a] bg-[#1f281c] text-[#c9d7a5]': answeredCurrentWord && !isCurrentGroupHomophones && w.id === trainingStore.currentWord?.word.id,
+                'border-[#d99165] bg-[#3a2419] text-[#f0b180]': answeredCurrentWord && !allWordsSelected && (isCurrentGroupHomophones || (selectedWordId === w.id && w.id !== trainingStore.currentWord?.word.id))
+              }">
+              {{ w.word }}
+            </li>
+          </ul>
+          <button @click="selectAllWords"
+            class="flex cursor-pointer gap-2 border border-stone-700 bg-[#0f0e0c] px-4 py-3 font-bold text-stone-100 transition-colors hover:border-[#9e553a] hover:text-[#d99165] rounded-xl"
+            :class="allWordsButtonClass">
+            All words sound the same
+          </button>
+        </div>
+        <button
+          class="mb-2 mt-16 cursor-pointer text-sm font-black uppercase tracking-[0.24em] text-stone-300 hover:text-[#d99165] disabled:cursor-not-allowed disabled:opacity-30"
+          @click="nextWord()" :disabled="!answeredCurrentWord">
+          {{ hasNextWord ? 'Next' : 'Finish' }}
+        </button>
+      </div>
+      <TrainingProgress />
+    </div>
   </template>
 </template>
